52e3df0a8b789c820ff7f1ce1b854fd3dc342174
Alumno <Alumno@PC46>
2025-05-07 12:51:52 -0300

    Agregar archivos de proyecto.

---
+++ pryRecio_20250507/FrmPrincipalH.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryRecio_20250507
+{
+    public partial class FrmPrincipalH : Form
+    {
+        public FrmPrincipalH()
+        {
+            InitializeComponent();
+        }
+    }
+}
